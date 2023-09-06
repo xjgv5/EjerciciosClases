0237f39a47182549cb80aa4bf2f42b46bfac639d
Javier Gomez <javier.gov@outlook.es>
2023-09-05 22:21:49 -0600

    Creacion de metodo detalle

--- src/Automovil.java
+++ src/Automovil.java
@@ -4,4 +4,11 @@ public class Automovil {
     String color = "gris";
     double cilindrado;
 
+    public void detalle(){
+        System.out.println("auto.fabricante = " + this.fabricante);
+        System.out.println("auto.modelo = " + this.modelo);
+        System.out.println("auto.color = " + this.color);
+        System.out.println("auto.cilindrado = " + this.cilindrado);
+    }
+
 }
--- src/EjemploAutomovil.java
+++ src/EjemploAutomovil.java
@@ -8,9 +8,6 @@ public class EjemploAutomovil {
         auto.cilindrado = 2.0;
         auto.color = "Blanco";
 
-        System.out.println("auto.fabricante = " + auto.fabricante);
-        System.out.println("auto.modelo = " + auto.modelo);
-        System.out.println("auto.color = " + auto.color);
-        System.out.println("auto.cilindrado = " + auto.cilindrado);
+        auto.detalle();
     }
 }
