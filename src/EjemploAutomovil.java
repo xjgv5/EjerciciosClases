c95d2974a89edf123f0fb5cdc8dc95db41165abf
Javier Gomez <javier.gov@outlook.es>
2023-09-26 16:00:28 -0600

    Metodo equals

--- src/EjemploAutomovil.java
+++ src/EjemploAutomovil.java
@@ -1,18 +1,20 @@
 public class EjemploAutomovil {
     public static void main(String[] args) {
 
-        Automovil auto = new Automovil("Subaru", "Impresa", "Blanco", 2.0, 40);
+        Automovil subaru = new Automovil("Subaru", "Impresa", "Blanco", 2.0, 40);
         Automovil mazda = new Automovil("Mazda", "BT-50", "Rojo", 3.0);
         Automovil nissan = new Automovil("Nissan", "Navara", "Gris oscuro", 3.5, 50);
+        Automovil auto = new Automovil();
 
-        System.out.println(auto.detalle());
+        System.out.println(subaru.detalle());
         System.out.println(mazda.detalle());
         System.out.println(nissan.detalle());
         System.out.println();
-        System.out.println(auto.acelerandoFrenar(4000));
+        System.out.println(subaru.acelerandoFrenar(4000));
+        System.out.println(auto.equals(subaru));
 
-        System.out.println("kilometros por litro " + auto.calcularConsumo(300, 0.75f));
-        System.out.println("kilometros por litro " + auto.calcularConsumo(300, 60));
+        System.out.println("kilometros por litro " + subaru.calcularConsumo(300, 0.75f));
+        System.out.println("kilometros por litro " + subaru.calcularConsumo(300, 60));
 
     }
 }
